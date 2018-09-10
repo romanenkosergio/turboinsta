c33dbb6ae81d56458c3c636aa8d4444d6062e7ad
Romanenko Sergio <sergromanenko97@gmail.com>
2018-09-10 16:01:59 +0300

    Внес измениния в отзывы
    Расширил кол-во показывающихся сразу скринов, теперь их 6. Ранее было 5

--- js/index.js
+++ js/index.js
@@ -156,7 +156,7 @@ $(document).ready(function() {
                 items: 3
             },
             1000: {
-                items: 5
+                items: 6
             }
         }
     })
