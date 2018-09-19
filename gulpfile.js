f5090ad08a7e337b585179290430e53334accc67
Romanenko Sergio <sergromanenko97@gmail.com>
2018-09-19 12:24:29 +0300

    Оптимизация
    Работа с Gulp img и js
    Оптимизация и сжатие файлов

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,34 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     cssmin = require('gulp-cssmin'),
     rename = require('gulp-rename');
+imagemin = require('gulp-imagemin');
+imageminJpegRecompress = require('imagemin-jpeg-recompress');
+pngquant = require('imagemin-pngquant');
+cache = require('gulp-cache');
+minifyjs = require('gulp-js-minify');
 
-
-gulp.task('sass', function () {  
-   return gulp.src('sass/*.sass')
+gulp.task('sass', function() {
+    return gulp.src('sass/*.sass')
         .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefixer(['last 15 versions']))
         .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
+        .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({stream: true}))
+        .pipe(browserSync.reload({ stream: true }))
 });
-
-gulp.task('browser-sync', function() {  
+gulp.task('minify-js', function() {
+    gulp.src('js/**/*')
+        .pipe(minifyjs())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+// Clearing the cache
+gulp.task('clear', function(done) {
+    return cache.clearAll(done);
+});
+gulp.task('browser-sync', function() {
     browserSync.init(["css/*.min.css", "js/*.js", "*.html"], {
         server: {
             baseDir: "./"
@@ -26,6 +40,6 @@ gulp.task('browser-sync', function() {
     });
 });
 
-gulp.task('default', ['sass', 'browser-sync'], function () {  
+gulp.task('default', ['sass', 'minify-js', 'clear', 'browser-sync'], function() {
     gulp.watch("sass/*.sass", ['sass']);
 });
